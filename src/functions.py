7567a445f0b890de7575373bb25172e4cc39ccf7
shaunildm <50808217+shaungudgud@users.noreply.github.com>
2020-06-01 09:37:31 -0700

    added concat functions

--- src/functions.py
+++ src/functions.py
@@ -32,13 +32,35 @@ class functionals:
     # instance attributes
     def __init__(self, path):
         self.path = path
-        
+        self.fig_id = fig_id
         
         
     # save figure
     def save_fig(self, fig_id, tight_layout=True):
-        path = os.path.join(fig_id + ".png") 
-        print("Saving figure", fig_id)
+        self.path = os.path.join(self.fig_id + ".png") 
+        print("Saving figure", self.fig_id)
         if tight_layout:
             plt.tight_layout()
-        plt.savefig(path, format='png', dpi=300)
+        plt.savefig(self.path, format='png', dpi=300)
+
+    # concatenate origin data
+    def orig_concatter(self):
+        self.files = glob.glob(self.path)
+
+        with open('orig.txt','w') as result:
+            for file_ in files:
+                for line in open(file_, 'r'):
+                    result.write(line)
+
+    # concatenate monthly performance data
+    def mp_concatter(self, indir, outdir):
+        filelist = glob.glob(indir)
+        dflist = []
+        for file in filelist:
+            print(file)
+            data = pd.read_csv(file, delimiter='|', names=mp_headers, low_memory=False)
+            data = data[cols_p]
+            data.dropna(inplace=True)
+            dflist.append(data)
+        concatdf = pd.concat(dflist, axis=0)
+        concatdf.to_csv(outdir, index=False)
